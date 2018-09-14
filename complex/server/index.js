85633aaab72541d0384dc9e97dd3a3841e628d3a
smalltide <smalltides@gmail.com>
2018-09-14 15:51:57 +0800

    add fib compute app with 6 docker services in complex folder

--- complex/server/index.js
+++ complex/server/index.js
@@ -55,6 +55,7 @@ app.post('/values', async (req, res) => {
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+  res.send('OK');
 });
 
 app.listen(5000, err => {
